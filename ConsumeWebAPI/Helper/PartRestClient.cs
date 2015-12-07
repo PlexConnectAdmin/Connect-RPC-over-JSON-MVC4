e5239debbf81a1bacf82c1a2a6bb4484fde3bdca
Tom Schulte <tsch@plex.com>
2015-12-07 11:34:35 -0500

    pre-alpha state of Details off parts grid

--- ConsumeWebAPI/Helper/PartRestClient.cs
+++ ConsumeWebAPI/Helper/PartRestClient.cs
@@ -42,7 +42,7 @@ namespace ConsumeWebAPI.Helper
       // Client app ID. 
       string clientId = _clientId;
 
-      // resource uri
+      // resource uri for the protected Plex resource you are accessing
       string resourceUri = tokenEndpoint.plexResource.ToString();
       AuthenticationResult authenticationResult;
 
@@ -70,10 +70,10 @@ namespace ConsumeWebAPI.Helper
 
       // 3. use bearer token to get some parts by status, etc. By default, this limits to 10 results
       //     At least of these controls must be specified: PartNo,Name,PartStatus,GroupKey,ProductTypeKey,CustomerNo,CustomerPart,DepartmentNo,PlannerKey,GradeKey,Master,ExclusiveUseRightsCustomerNo,PartDrawingNumber,PartProductGroupKey
-      // todo: put this filter in the UI, or a config setting or something
-      
+
       // other sample call
       // RestClient restClient = new RestClient("https://test.api.plex.com/Engineering/PartList/Parts?partType=Other&PartStatus=Production");
+      // todo: put this filter in the UI, or a config setting or something
       RestClient restClient = new RestClient("https://test.api.plex.com/Engineering/PartList/Parts?PartNo=z");
       RestRequest request = new RestRequest(Method.GET);
       request.AddHeader("cache-control", "no-cache");
@@ -84,37 +84,88 @@ namespace ConsumeWebAPI.Helper
       JToken jsonVal = JToken.Parse(response.Content);
 
       // todo: response has links "next" and "last" (previous) for paging: ...offset=380&limit=10... etc. Implement this paging.
-      
+
       JArray joResponses = ((dynamic)jsonVal).embedded;
 
       List<PartModel> Parts = null;
-      if(joResponses ==null)
+      if (joResponses == null)
       {
-      // todo: handle no parts returned
+        // todo: handle no parts returned
       }
       else
       {
-
-      Parts = joResponses.ToObject<List<PartModel>>();
+        Parts = joResponses.ToObject<List<PartModel>>();
       }
 
-      return Parts;
-
       // todo: try/catch for error handling on all code
+
+      return Parts;
     }
 
     public PartModel GetById(int id)
     {
-      var request = new RestRequest("api/part/{id}", Method.GET) { RequestFormat = DataFormat.Json };
+      // 0. Get settings from JSON config file
+      JToken bearerToken;
+      string exeRuntimeDirectory = System.IO.Path.GetDirectoryName(HttpRuntime.AppDomainAppPath);
+      TokenEndpoint tokenEndpoint = JsonConvert.DeserializeObject<TokenEndpoint>(File.ReadAllText(exeRuntimeDirectory + @"\PlexConnectConfig.json"));
 
-      request.AddParameter("id", id, ParameterType.UrlSegment);
+      _tokenEndpoint = tokenEndpoint.tokenEndPoint;
+      _clientId = tokenEndpoint.clientId;
 
-      var response = _client.Execute<PartModel>(request);
+      // use the secret with the furthest out expiry. Use the JSON configuration file to manage your rolling secrets 
+      List<ClientSecret> SortedClientSecretList = tokenEndpoint.clientSecrets.OrderByDescending(o => o.expiry).ToList();
 
-      if (response.Data == null)
-        throw new Exception(response.ErrorMessage);
+      // 1. get the bearer token
 
-      return response.Data;
+      // Client app ID. 
+      string clientId = _clientId;
+
+      // resource uri for the protected Plex resource you are accessing
+      string resourceUri = tokenEndpoint.plexResource.ToString();
+      AuthenticationResult authenticationResult;
+
+      // OAuth2 token endpoint for 2-legged, server-to-server application authorization
+      string authorityUri = _tokenEndpoint + "?grant_type=client_credentials";
+
+      // Create a new AuthenticationContext passing an Authority URI.
+      Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext authContext = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(authorityUri);
+
+      string clientSecret = SortedClientSecretList[0].clientSecret;
+      ClientCredential clientCredential = new ClientCredential(clientId, clientSecret);
+
+      // 2. extract bearer token from AuthenticationResult
+      try
+      {
+        // AAD includes an in memory cache, so this call may only receive a new token from the server if the cached token is expired.
+        authenticationResult = authContext.AcquireToken(resourceUri, clientCredential);
+        bearerToken = authenticationResult.AccessToken;
+      }
+      catch (Exception e)
+      {
+        // handle any error
+        throw e;
+      }
+
+      // 3. use bearer token and HTTP Method DELETE to delete the part. There is no confirmation before execution
+      RestClient restClient = new RestClient("https://test.api.plex.com/Engineering/PartList/Parts/" + id.ToString());
+      RestRequest request = new RestRequest(Method.GET);
+      request.AddHeader("cache-control", "no-cache");
+      request.AddHeader("authorization", "Bearer " + bearerToken);
+      request.AddHeader("content-type", "multipart/form-data");
+      IRestResponse response = restClient.Execute(request);
+
+      PartModel Part = null;
+      if (response == null)
+      {
+        // todo: handle no parts returned
+      }
+      else
+      {
+        JToken jsonVal = JToken.Parse(response.Content);
+        Part = jsonVal.ToObject<PartModel>();
+      }
+
+      return Part;
     }
 
     public PartModel GetByType(int type)
@@ -167,10 +218,57 @@ namespace ConsumeWebAPI.Helper
 
     public void Delete(int id)
     {
-      var request = new RestRequest("api/part/{id}", Method.DELETE);
-      request.AddParameter("id", id, ParameterType.UrlSegment);
+      // 0. Get settings from JSON config file
+      JToken bearerToken;
+      string exeRuntimeDirectory = System.IO.Path.GetDirectoryName(HttpRuntime.AppDomainAppPath);
+      TokenEndpoint tokenEndpoint = JsonConvert.DeserializeObject<TokenEndpoint>(File.ReadAllText(exeRuntimeDirectory + @"\PlexConnectConfig.json"));
 
-      var response = _client.Execute<PartModel>(request);
+      _tokenEndpoint = tokenEndpoint.tokenEndPoint;
+      _clientId = tokenEndpoint.clientId;
+
+      // use the secret with the furthest out expiry. Use the JSON configuration file to manage your rolling secrets 
+      List<ClientSecret> SortedClientSecretList = tokenEndpoint.clientSecrets.OrderByDescending(o => o.expiry).ToList();
+
+      // 1. get the bearer token
+
+      // Client app ID. 
+      string clientId = _clientId;
+
+      // resource uri for the protected Plex resource you are accessing
+      string resourceUri = tokenEndpoint.plexResource.ToString();
+      AuthenticationResult authenticationResult;
+
+      // OAuth2 token endpoint for 2-legged, server-to-server application authorization
+      string authorityUri = _tokenEndpoint + "?grant_type=client_credentials";
+
+      // Create a new AuthenticationContext passing an Authority URI.
+      Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext authContext = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(authorityUri);
+
+      string clientSecret = SortedClientSecretList[0].clientSecret;
+      ClientCredential clientCredential = new ClientCredential(clientId, clientSecret);
+
+      // 2. extract bearer token from AuthenticationResult
+      try
+      {
+        // AAD includes an in memory cache, so this call may only receive a new token from the server if the cached token is expired.
+        authenticationResult = authContext.AcquireToken(resourceUri, clientCredential);
+        bearerToken = authenticationResult.AccessToken;
+      }
+      catch (Exception e)
+      {
+        // handle any error
+        throw e;
+      }
+
+      // 3. use bearer token and HTTP Method DELETE to delete the part. There is no confirmation before execution
+      RestClient restClient = new RestClient("https://test.api.plex.com/Engineering/PartList/Parts/" + id.ToString());
+      RestRequest request = new RestRequest(Method.DELETE);
+      request.AddHeader("cache-control", "no-cache");
+      request.AddHeader("authorization", "Bearer " + bearerToken);
+      request.AddHeader("content-type", "multipart/form-data");
+      IRestResponse response = restClient.Execute(request);
+
+      JToken jsonVal = JToken.Parse(response.Content);
 
       if (response.StatusCode == HttpStatusCode.NotFound)
       {
